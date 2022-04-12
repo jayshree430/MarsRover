85ccb90dc685981b8b187b7af7b0ecff705432b5
jayshree430 <iyer430@gmail.com>
2022-04-12 22:27:25 +0100

    Created new parameterized test to check for NullOrEmpty string as well as movements in L and R directions. Added new methods in Rover class to implement left, right and processMovement

--- src/main/java/com/Jayshree/MarsRover/Rover.java
+++ src/main/java/com/Jayshree/MarsRover/Rover.java
@@ -4,15 +4,16 @@ public class Rover {
 
     private int posX;
     private int posY;
-    Direction d;
+    Direction direction;
+    private Plateau plateau;
 
-    public Rover(int posX, int posY, Direction d) throws IllegalArgumentException{
+    public Rover(int posX, int posY, Direction direction) throws IllegalArgumentException{
         if(posX <= 0 || posY <= 0){
             throw new IllegalArgumentException("Position should be greater than Zero");
         }
         this.posX = posX;
         this.posY = posY;
-        this.d = d;
+        this.direction = direction;
     }
 
     public int getPosX(){
@@ -21,5 +22,50 @@ public class Rover {
     public int getPosY(){
         return posY;
     }
+    public Direction getDirection(){
+        return direction;
+    }
+    public String processMovement(String instruction){
+        if (instruction == null || instruction.isEmpty() || instruction.equals(" ")){
+            return "Instructions cannot be Empty or null";
+        }
+        if (!instruction.matches("^[LMRlmr]+$")) {
+            return "Invalid Instructions. Please provide only L, R or M as input";
+        }
+        String[] instructionArray = instruction.split("");
+        for (String input : instructionArray) {
+            try {
+                switch (input.toUpperCase()) {
+                    case "L" -> goLeft();
+                    case "R" -> goRight();
+                    default -> System.out.println("Something else? " + input);
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return "Rover moved to new direction";
+    }
+
+    private void goLeft(){
+        switch (direction) {
+            case EAST -> direction = Direction.NORTH;
+            case SOUTH -> direction = Direction.EAST;
+            case WEST -> direction = Direction.SOUTH;
+            case NORTH -> direction = Direction.WEST;
+            default -> System.out.println("Direction = " + direction);
+        }
+    }
+
+        private void goRight() {
+            switch (direction) {
+                case EAST -> direction = Direction.SOUTH;
+                case SOUTH -> direction = Direction.WEST;
+                case WEST -> direction = Direction.NORTH;
+                case NORTH -> direction = Direction.EAST;
+                default -> System.out.println("Direction = " + direction);
+            }
+
+        }
 
 }
--- src/test/java/com/Jayshree/MarsRover/RoverTest.java
+++ src/test/java/com/Jayshree/MarsRover/RoverTest.java
@@ -1,8 +1,11 @@
 package com.Jayshree.MarsRover;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class RoverTest {
 
@@ -31,10 +34,35 @@ public class RoverTest {
         assertEquals(message, exceptionMessage);
     }
     @Test
-    public void checkPosition(){
+    public void checkPositionAndDirection(){
         rover = new Rover(3,3,Direction.EAST);
         assertEquals(3, rover.getPosX());
         assertEquals(3, rover.getPosY());
+        assertEquals(Direction.EAST, rover.getDirection());
+    }
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" "})
+    public void isBlank_ShouldReturnFalseForNullOrBlankStrings(String instruction){
+        rover = new Rover(3, 5, Direction.EAST);
+        String expected = "Instructions cannot be Empty or null";
+        assertEquals(expected,rover.processMovement(instruction));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"L", "R", "l", "r"})
+    public void move_ShouldTurnToNewDirection(String instruction){
+        rover = new Rover(3, 5, Direction.EAST);
+        String expected = "Rover moved to new direction";
+        assertEquals(expected,rover.processMovement(instruction));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"t", "S", "K"})
+    public void move_InvalidInput(String instruction){
+        rover = new Rover(3, 5, Direction.EAST);
+        String expected = "Invalid Instructions. Please provide only L, R or M as input";
+        assertEquals(expected,rover.processMovement(instruction));
     }
 
 
