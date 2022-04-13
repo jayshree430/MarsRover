95da0b36c740fec4c827124cb441bccc0856f5a6
jayshree430 <iyer430@gmail.com>
2022-04-13 02:00:53 +0100

    Created getCommandNumbers() and getCommand() to get plateau size from user and check if they are valid and seperated by space

--- src/main/java/com/Jayshree/MarsRover/Main.java
+++ src/main/java/com/Jayshree/MarsRover/Main.java
@@ -1,13 +1,39 @@
 package com.Jayshree.MarsRover;
 
-public class Main {
-    public static void main(String[] args) {
-        Plateau plateau = new Plateau(7, 7);
-        Rover rover = new Rover(1, 1, Direction.SOUTH, plateau);
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-        System.out.println("Initial Position of the rover: x" + rover.getPosX() + " y" + rover.getPosY() + " " + rover.getDirection());
-        String result = rover.processMovement("MMMLM");
-        System.out.println("Position of the rover: x" + rover.getPosX() + " y" + rover.getPosY() + " " + rover.getDirection());
-        System.out.println(result);
+public class Main {
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Mars!");
+        System.out.print("Please enter the size of Plateau. Enter two numbers separated by a space: ");
+        int[] plateauSize = getCommandNumbers();
+        Plateau plateau = new Plateau(plateauSize[0], plateauSize[1]);
+    }
+    public static int[] getCommandNumbers(){
+        String size;
+        boolean hasSize = false;
+        do {
+            size = getCommand();
+            if (!size.matches("^[0-9]+( [0-9]+)")) {
+                System.out.println("Must be two numbers separated by a single space.");
+            }
+            else{
+                hasSize = true;
+            }
+        } while (!hasSize);
+        String[] plateauSize = size.split(" ");
+        return new int[]{Integer.parseInt(plateauSize[0]), Integer.parseInt(plateauSize[1])};
+    }
+    private static String getCommand() {
+        String movement = "";
+        try {
+            movement = reader.readLine();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return movement;
     }
 }
